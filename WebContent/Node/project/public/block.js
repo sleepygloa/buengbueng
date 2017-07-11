b0101c2c39469acb5453fc84b0883c09072fa53a
ko <hagu03@gmail.com>
2017-07-12 03:17:25 +0900

    - 챗봇 CSS 수정 - 챗봇 html 안 js 밖으로 빼냄

--- WebContent/Node/project/public/block.js
+++ WebContent/Node/project/public/block.js
@@ -1,4 +1,61 @@
 $(document).ready(function(){
+//  var socket = io.connect('ws://192.168.0.206:3000');
+    var socket = io.connect('ws://localhost:3000');
+    $(document).ready(function(){
+      // 입력버튼 누르면 발생
+      $('#submitBtn').click(function(){
+        var keyword = "";
+        // 사용자 질문 채팅창에 띄우기
+        var txt = $('#chat').html();
+		  var userTxt = $('#userTxt').val().replace("\n","<br/>");
+        $('#chat').html(
+		  		txt+"<div class='line'>"+
+						"<div class='user'>사용자<br/>"+
+							"<div class='question'>"+
+								userTxt+
+							"</div>"+
+						"</div>"+
+					"</div>"
+		  );
+        var question =  document.getElementById("userTxt").value;
+        // 사용자의 질문에서 키워드 추출하기
+        $.ajax({
+      		url:"http://api.datamixi.com/datamixiApi/keywordextract",
+      		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+      		type:"POST",
+      		dataType:'text',
+      		data: {
+      			request:'post',
+      			key:'7173114780022473721',
+      			text: question
+      		},
+          success: function(data){
+      			var parser = JSON.parse(data);
+      			for(i=0;i<parser.return_object.length;i++){
+      				var last = parser.return_object[i].term.indexOf('|');
+      				keyword += parser.return_object[i].term.substring(0,last);
+              if(i != parser.return_object.length-1){
+                keyword  += ",";
+              }
+      			}
+            socket.emit('chat', keyword,question);
+      		}
+      	});
+        socket.on('recieveChat',function(data){
+          var txt = $('#chat').html();
+          $('#chat').html(
+				txt+"<div class='line'>"+
+						"<div class='admin'>관리자<br/>"+
+							"<div class='answer'>"+
+								data+
+							"</div>"+
+						"</div>"+
+					"</div>"
+			);
+          socket.end();
+        });
+      });
+    });
 	$(document).bind('keydown',function(e){
 		if (e.keyCode == 123) { /* F12 */
 			e.preventDefault();
